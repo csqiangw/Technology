872a44638e951a785f8efcf592ae5d6a01d79821
csqiangw <321516900@qq.com>
2023-04-25 16:38:31 +0800

    一级缓存测试命名修改

--- mybatis/src/main/java/com/example/OneLevelCacheTest.java
+++ mybatis/src/main/java/com/example/OneLevelCacheTest.java
@@ -6,14 +6,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class cacheTest {
+public class OneLevelCacheTest {
 
     public SqlSessionFactory factory;
 
